3d7b33a370096ac0a4b3aee63172779a0c1e6b60
evaburgering <112861873+evaburgering@users.noreply.github.com>
2023-09-29 17:21:28 +0200

    Add files via upload

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -21,3 +21,22 @@ document.addEventListener('scroll', function () {               //selecteert het
         first_header.classList.add('hide') // voegt de verzamelde classes die Hide heten weer toe
     }
 });
+
+
+
+
+// Darkmode met hulp van bron: Jordy mijn zwager en student grogrammeur
+const toggle = document.getElementById('dark-mode-toggle'); //toggle word aangemaakt en gelinkt aan t element dark-mode-toggle
+const body = document.body; 
+
+// Functie om dark mode in- of uit te schakelen
+function toggleDarkMode() {   // hier word de functie toggleDarkMode gedefinieerd.Deze functie wordt later aangeroepen wanneer de checkbox voor dark mode wordt aangevinkt.
+    if (toggle.checked) {     // deze fuctie controleerd of checkbox met id 'dark-mode-toggle' is ingeschakeld (aangevinkt).
+        body.classList.add('dark-mode');  //als dit zo is word de dark mode toegevoegd
+    } else {
+        body.classList.remove('dark-mode'); //zo niet word deze weg gehaald.
+    }
+}
+
+// Voegt een eventlistener toe aan de checkbox om dark mode te beheren. of er iets verandrd en hij wat moet doen of niet
+toggle.addEventListener('change', toggleDarkMode);
